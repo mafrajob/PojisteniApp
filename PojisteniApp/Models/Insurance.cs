0a816507a5699a4dd095483c01723afc4d515d7a
Martin Franc <mafra.job@gmail.com>
2023-04-04 12:31:23 +0200

    Added attributes to Insurance and Person Models' properties.

--- PojisteniApp/Models/Insurance.cs
+++ PojisteniApp/Models/Insurance.cs
@@ -1,17 +1,28 @@
-﻿namespace PojisteniApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PojisteniApp.Models
 {
     public class Insurance
     {
         public int InsuranceId { get; set; }
-
+        
+        [Display(Name = "Pojištění")]
         public InsuranceType InsuranceType { get; set; } = new InsuranceType();
 
+        [Display(Name = "Částka")]
         public int InsuranceAmount { get; set; }
 
+        [Display(Name = "Předmět pojištění")]
+        [Required(ErrorMessage = "Vyplňte předmět pojištění")]
+        [StringLength(150, ErrorMessage = "Maximální délka předmětu pojištění je 150 znaků")]
         public string InsuranceSubject { get; set; } = string.Empty;
 
+        [Display(Name = "Platnost od")]
+        [Required(ErrorMessage = "Vyplňte platnost od")]
         public DateTime ValidFrom { get; set; } = DateTime.MinValue;
 
+        [Display(Name = "Platnost do")]
+        [Required(ErrorMessage = "Vyplňte platnost do")]
         public DateTime ValidTo { get; set; } = DateTime.MaxValue;
     }
 }
