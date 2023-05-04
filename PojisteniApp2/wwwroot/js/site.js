07b77c1a44598442e1bda1c15a2e3604eede3db2
Martin Franc <mafra.job@gmail.com>
2023-05-04 20:04:00 +0200

    Added image size validation to javascript.

--- PojisteniApp2/wwwroot/js/site.js
+++ PojisteniApp2/wwwroot/js/site.js
@@ -4,15 +4,22 @@
 // Image preview based on tutorial: https://w3collective.com/preview-selected-img-file-input-js/
 const chooseImg = document.getElementById("choose-img");
 const profileImg = document.getElementById("profile-img");
+const validationImg = document.getElementById("validation-img");
 
 function getImgData() {
     const image = chooseImg.files[0];
     if (image) {
-        const fileReader = new FileReader();
-        fileReader.readAsDataURL(image);
-        fileReader.addEventListener("load", function () {
-            profileImg.src = this.result
-        });
+        if ((image.size / (1024 * 1024)) > 1) {
+            validationImg.innerText = "Maximální velikost obrázku je 1 MB";
+        }
+        else {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(image);
+            fileReader.addEventListener("load", function () {
+                profileImg.src = this.result;
+            });
+            validationImg.innerText = "";
+        }
     }
 }
 
