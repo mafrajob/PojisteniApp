d84b87369767af514c1f81345069038a7adf550d
Martin Franc <mafra.job@gmail.com>
2023-05-05 09:01:34 +0200

    Moved person image preview and size validation to the separate .js file.

---
+++ PojisteniApp2/wwwroot/js/person-image.js
@@ -0,0 +1,26 @@
+﻿// Image preview based on tutorial: https://w3collective.com/preview-selected-img-file-input-js/
+const chooseImg = document.getElementById("choose-img");
+const profileImg = document.getElementById("profile-img");
+const validationTextImg = document.getElementById("validation-text-img");
+const maxSizeImg = parseFloat(document.getElementById("max-size-img").value);
+
+function getImgData() {
+    const image = chooseImg.files[0];
+    if (image) {
+        if ((image.size / (1024 * 1024)) > maxSizeImg) {
+            validationTextImg.innerText = `Maximální velikost obrázku je ${maxSizeImg} MB`;
+        }
+        else {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(image);
+            fileReader.addEventListener("load", function () {
+                profileImg.src = this.result;
+            });
+            validationTextImg.innerText = "";
+        }
+    }
+}
+
+chooseImg.addEventListener("change", function () {
+    getImgData();
+});
--- PojisteniApp2/wwwroot/js/site.js
+++ PojisteniApp2/wwwroot/js/site.js
@@ -1,29 +1,4 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Image preview based on tutorial: https://w3collective.com/preview-selected-img-file-input-js/
-const chooseImg = document.getElementById("choose-img");
-const profileImg = document.getElementById("profile-img");
-const validationTextImg = document.getElementById("validation-text-img");
-const maxSizeImg = parseFloat(document.getElementById("max-size-img").value);
-
-function getImgData() {
-    const image = chooseImg.files[0];
-    if (image) {
-        if ((image.size / (1024 * 1024)) > maxSizeImg) {
-            validationTextImg.innerText = `Maximální velikost obrázku je ${maxSizeImg} MB`;
-        }
-        else {
-            const fileReader = new FileReader();
-            fileReader.readAsDataURL(image);
-            fileReader.addEventListener("load", function () {
-                profileImg.src = this.result;
-            });
-            validationTextImg.innerText = "";
-        }
-    }
-}
-
-chooseImg.addEventListener("change", function () {
-    getImgData();
-});
+// Add your JavaScript code
