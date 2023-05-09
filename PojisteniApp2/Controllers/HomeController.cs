287481dd80ff1efb2a31f6f61b9e29cfe93dc8e4
Martin Franc <mafra.job@gmail.com>
2023-05-10 00:06:45 +0200

    Added stats and controller actions to home carousel.

--- PojisteniApp2/Controllers/HomeController.cs
+++ PojisteniApp2/Controllers/HomeController.cs
@@ -1,20 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PojisteniApp2.Models;
 using System.Diagnostics;
+using PojisteniApp2.Data;
 
 namespace PojisteniApp2.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            ViewBag.PersonCount = _context.Person.Count();
+            int insuranceCount = _context.Insurance.Count();
+            ViewBag.InsuranceCount = insuranceCount;
+            int maxInsuranceLimit = 0;
+            if (insuranceCount > 0)
+            {
+                // This LINQ query throws exception when no insurances in the DB
+                maxInsuranceLimit = _context.Insurance.Max(i => i.InsuranceAmount);
+            }
+            // https://stackoverflow.com/questions/3456926/how-to-insert-a-thousand-separator-comma-with-convert-to-double
+            ViewBag.MaxInsuranceLimit = maxInsuranceLimit.ToString("N");
+
             return View();
         }
 
