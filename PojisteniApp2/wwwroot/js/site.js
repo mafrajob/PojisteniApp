3ca0943c5b17cdfb18511cece93bf37a3c98f83a
Martin Franc <mafra.job@gmail.com>
2023-05-15 11:20:22 +0200

    Added tooltip for disabled buttons.

--- PojisteniApp2/wwwroot/js/site.js
+++ PojisteniApp2/wwwroot/js/site.js
@@ -4,14 +4,18 @@
 // Add your JavaScript code
 
 // Disable select by Tab key for all elements with class disabled
-function disableForTabKey() {
+// Add title to it's wrapper
+function adjustDisabledElements() {
     const disabledElements = document.getElementsByClassName("disabled");
     for (const element of disabledElements) {
         element.setAttribute("tabindex", "-1");
+        let titleWrapper = element.closest("span.disabled-title-wrapper");
+        if (titleWrapper) {
+            titleWrapper.setAttribute("title", "Pouze pro přihlášeného uživatele, autora nebo administrátora");
+            titleWrapper.setAttribute("tabindex", "0");
+        }
     }
 }
 
 //document.onload event not fired as expected, maybe because of ASP.NET CORE Razor View Engine
-document.addEventListener("DOMContentLoaded", function () {
-    disableForTabKey();
-});
+document.addEventListener("DOMContentLoaded", adjustDisabledElements());
